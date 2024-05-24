202ef6842c0ea4e8f4628f3a5941a748593fb626
Your Name <you@example.com>
2024-05-24 18:54:14 -0500

    fix: absolute position nav on Continue view was borked for next button

--- src/components/Continue.jsx
+++ src/components/Continue.jsx
@@ -4,10 +4,11 @@ import { MainMenu } from "./TitleScreen";
 import { usePlayer } from "../context/player";
 import { Button } from "../styles/Button";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Continue = forwardRef((props, ref) => {
 
-    const { getPlayerData, switchPlayer } = usePlayer();
+    const { getPlayerData, switchPlayer, activePlayerId } = usePlayer();
     const { saveFiles } = getPlayerData();
 
     return (
@@ -21,15 +22,26 @@ const Continue = forwardRef((props, ref) => {
                 <br />
             </MainMenu>
             <NavAbsolutePosition>
-                <Link className="prev" to={"/"}>BACK</Link>
+                <StyledLink className="prev" to={"/"} position="left">BACK</StyledLink>
+                {activePlayerId !== 'character-creation' &&
+                    <StyledLink className="next" to={"/"} position="right">NEXT</StyledLink>
+                }
             </NavAbsolutePosition>
         </section>
     )
 });
 
+
 const NavAbsolutePosition = styled.nav`
+    position: relative;
+    margin-top: -70px;
+`;
+
+const StyledLink = styled(Link)`
     position: absolute;
-    bottom: '5%';
+    bottom: 5%;
+    ${props => props.position}: 0;
+    margin: 0 10px;
 `;
 
 export default Continue;
