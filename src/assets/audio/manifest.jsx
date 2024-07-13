cbe468bc021f8e390dc538baade07cfcdd05ed06
Your Name <you@example.com>
2024-07-13 14:31:40 -0500

    hopefully fix audio for prod

--- src/assets/audio/manifest.jsx
+++ src/assets/audio/manifest.jsx
@@ -1,28 +1,30 @@
+const audioFiles = import.meta.glob('/src/assets/audio/**/*.{mp3,flac,ogg,webm}', { eager: true });
+
 const audioDir = '/src/assets/audio';
 
 const audioManifest = {
     NEON_WORLD: {
-        src: audioDir + '/bgm/neon_world.mp3',
+        src: audioFiles[audioDir + '/bgm/neon_world.mp3'].default,
         volume: 0.75
     },
     W96_ALONEARMED: {
-        src: audioDir + '/event/W96_ALONEARMED.mp3',
-        volume:  1.0
+        src: audioFiles[audioDir + '/event/W96_ALONEARMED.mp3'].default,
+        volume: 1.0
     },
     SPACE: {
-        src: audioDir + '/bgm/space.flac',
+        src: audioFiles[audioDir + '/bgm/space.flac'].default,
         volume: 0.85
     },
     SOUND: {
-        src: audioDir + '/sfx/sound.flac',
+        src: audioFiles[audioDir + '/sfx/sound.flac'].default,
         volume: 5.0
     },
     TOTHEGREATBEYOND: {
-        src: audioDir + '/bgm/stellardrone-tothegreatbeyond.ogg',
+        src: audioFiles[audioDir + '/bgm/stellardrone-tothegreatbeyond.ogg'].default,
         volume: 0.75
     },
     SLOWSPACE: {
-        src: audioDir + '/bgm/efence-slowspace.webm',
+        src: audioFiles[audioDir + '/bgm/efence-slowspace.webm'].default,
         volume: 0.75
     }
 }
